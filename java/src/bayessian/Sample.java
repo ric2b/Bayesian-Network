de4e0cb5a6a8f80bfa2fe36b67ea858a75538d98
davidfialho <fialho.david@gmail.com>
2015-05-08 21:41:31 +0100

    samples passam a ter todas um comprimento fixo igual para todas adicionado método para determinar comprimento das samples

--- java/src/bayessian/Sample.java
+++ java/src/bayessian/Sample.java
@@ -1,15 +1,28 @@
 package bayessian;
 
+import java.util.Arrays;
+
 public class Sample {
 	
-	int[] values = null; //vector de inteiros que corresponde a uma linha de uma time slice
-
-	public Sample(int size) {
-		values = new int[size];
-	}
+	private static int length = -1;		// todas as amostram têm o mesmo comprimento; este valor é determinado
+										// quando se cria o primeiro objecto da classe Sample
+	int[] values = null; 				// vector de inteiros que corresponde a uma linha de uma time slice
 	
-	public Sample(int[] valuesArg) {
-		values = valuesArg;
+	public Sample(int[] values) {
+		if(values.length == 0) {
+			throw new IllegalArgumentException("o array values deve ter um comprimento superior a zero");
+		}
+		
+		if(Sample.length == -1) {
+			// o comprimento das samples ainda não está definido
+			// usar comprimento do array recebido
+			Sample.length = values.length;
+		}
+		
+		// é criado um array de Sample.length posições
+		// caso values tenha menos valores que Sample.length os restantes são preenchidos com zeros
+		// caso values tenha mais valores que Sample.length apenas são copiados os valores necessários
+		this.values = Arrays.copyOfRange(values, 0, Sample.length);
 	}
 	
 	public void setValue(int index, int value) {
@@ -18,5 +31,25 @@ public class Sample {
 	
 	public int getValue(int index) {
 		return values[index];
+	}
+	
+	public int length() {
+		return Sample.length;
+	}
+	
+	public static void main(String[] args) {
+		Sample s1 = new Sample(new int[10]);
+		
+		System.out.println("sample 1");
+		for(int i = 0; i < Sample.length; i++) {
+			System.out.println(s1.getValue(i));
+		}
+		
+		Sample s2 = new Sample(new int[1]);
+		
+		System.out.println("sample 2");
+		for(int i = 0; i < Sample.length; i++) {
+			System.out.println(s2.getValue(i));
+		}
 	}
 }
