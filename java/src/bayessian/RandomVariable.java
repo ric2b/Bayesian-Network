2a4df3fbe8a23388776fd328ff477b58012b61f4
davidfialho <fialho.david@gmail.com>
2015-05-15 00:14:49 +0100

    feito override do equals e do hascode da RandomVariable

--- java/src/bayessian/RandomVariable.java
+++ java/src/bayessian/RandomVariable.java
@@ -3,7 +3,7 @@ package bayessian;
 import java.util.Iterator;
 
 public abstract class RandomVariable implements Iterable<Integer> {
-	
+
 	protected String name = null;
 	protected int range = -1;
 	protected int timeInstant = -1;
@@ -37,4 +37,35 @@ public abstract class RandomVariable implements Iterable<Integer> {
 	public abstract Iterator<Integer> iterator();
 	
 	public abstract int getValueCount();
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + range;
+		result = prime * result + timeInstant;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		if(!(obj instanceof RandomVariable))
+			return false;
+		
+		RandomVariable other = (RandomVariable) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (range != other.range)
+			return false;
+		if (timeInstant != other.timeInstant)
+			return false;
+		return true;
+	}
+	
 }
