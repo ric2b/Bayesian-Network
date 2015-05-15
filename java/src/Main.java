e789736eb8630150530464dfe9e3786ce2b7067f
davidfialho <fialho.david@gmail.com>
2015-05-15 22:19:21 +0100

    incluido main geral para testar varios componentes

---
+++ java/src/Main.java
@@ -0,0 +1,34 @@
+import input.DataFileReader;
+
+import java.util.Arrays;
+
+import dataset.TimeSlice;
+import dataset.TransitionDataset;
+import bayessian.RandomVariable;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		DataFileReader reader = new DataFileReader("short-train-data.csv");
+		
+		System.out.println("#Subjects: " + reader.subjectCount());
+		System.out.println("#RVars: " + reader.randomVarCount());
+		System.out.println("#TimeInstants: " + reader.timeInstantCount());
+		
+		int instantCount = reader.timeInstantCount();
+		TimeSlice[] timeSlices = new TimeSlice[instantCount];
+		
+		for(int i = 0; i < instantCount; i++) {
+			timeSlices[i] = reader.getTimeSlice(i);
+			
+			System.out.println("TimeSlice " + i);
+			System.out.println(timeSlices[i]);
+		}
+		
+		RandomVariable[] vars = reader.getRVars();
+		System.out.println(Arrays.toString(vars));
+		
+		TransitionDataset dataset = new TransitionDataset(timeSlices);
+		
+		System.out.println(dataset);
+	}
+}
