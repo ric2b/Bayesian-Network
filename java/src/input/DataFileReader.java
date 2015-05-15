ef3991652a0b07bcf441d153fbfabfbab65e2ff4
davidfialho <fialho.david@gmail.com>
2015-05-15 19:31:59 +0100

    modificado data file reader de forma a funcionar para test files neste caso as variaveis aleatorias passam a ser distinguidas para os varios instantes de tempo

--- java/src/input/DataFileReader.java
+++ java/src/input/DataFileReader.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 import dataset.Sample;
 import dataset.TimeSlice;
-import bayessian.RandomVector;
+import bayessian.RandomVariable;
+import bayessian.StaticRandomVariable;
 
 public class DataFileReader {
 	
@@ -28,53 +29,93 @@ public class DataFileReader {
 		return 1 + Integer.parseInt(lastElement.substring(indexOfUnderScore + 1, lastElement.length()));
 	}
 	
+	/**
+	 * Devolve numero de linhas dos dados do ficheiro de entrada
+	 * @return
+	 */
 	public int subjectCount() {
 		return fileReader.getRowCount() - 1; 
 	}
 	
-	public int randomVarCount() {	
-		return firstLine.length / timeInstantCount();
+	/**
+	 * Devolve o numero de variáveis aleatorias no ficheiro de entrada. Uma variavel aleatoria é identificada pelo seu
+	 * nome e instante de tempo.
+	 * @return
+	 */
+	public int randomVarCount() {
+		// numero de elementos da primeira linha
+		return firstLine.length;
 	}
 	
-	public RandomVector getRVars() {		
+	/**
+	 * Obtem um array com todas as variaveis aleatorias do ficheiro. Duas variaveis aleatorias com o mesmo nome em dois 
+	 * instantes de tempo diferentes sao consideradas diferentes
+	 * @return
+	 */
+	public RandomVariable[] getRVars() {		
 		
-		String[] nameRVars = null;
-		int[] rangeRVars = null;
 		int numberOfRVars = randomVarCount();
+		RandomVariable[] vars = new RandomVariable[numberOfRVars];
+		String varName = null;		// nome da variavel
+		int timeInstant = -1;		// instante de tempo da variavel
 		
-		if(nameRVars == null) {
-			nameRVars = new String[numberOfRVars];
-			rangeRVars = new int[numberOfRVars];
-			for(int i = 0; i < numberOfRVars; i++) {
-				nameRVars[i] = firstLine[i].substring(0, firstLine[i].lastIndexOf("_0")); //procurar ultima ocorrencia de "_0"
-				rangeRVars[i] = getRangeSingleRVar(i, numberOfRVars);
+		int[] ranges = getRanges();
+		
+		for(int i = 0; i < numberOfRVars; i++) {
+			// obter indice do underscore
+			int indexOfUnderScore = firstLine[i].lastIndexOf("_");
+			
+			if(indexOfUnderScore == -1) {
+				// dados de um test file que só têm 1 instante de tempo
+				varName = firstLine[i];
+				timeInstant = 0;
+			} else {
+				// nome da variavel é dado apenas pela parte à esquerda do underscore
+				varName = firstLine[i].substring(0, indexOfUnderScore);
+				// instante de tempo da variavel é dado pela o numero à direita do underscore
+				timeInstant = Integer.parseInt(firstLine[i].substring(indexOfUnderScore + 1, firstLine[i].length()));
 			}
+			
+			vars[i] = new StaticRandomVariable(varName, ranges[i / numberOfRVars], timeInstant);
 		}
-		
-		RandomVector randomVector = new RandomVector(nameRVars, rangeRVars); //chamar construtor do RandomVector
-		
-		return randomVector;
+
+		return vars;
 	}
 	
-	private int getRangeSingleRVar(int varIndex, int numberOfRVars) {
-		int sample = 0, max = 0;
-		int numberOfSubjects = subjectCount();
+	/**
+	 * Permite obter todos os ranges de todas as variaveis aletorais de forma sequencial. Preferivel a fazê-lo uma a uma.
+	 * @return range das variaveis aletorias pela mesma odem que se encontram no ficheiro
+	 */
+	private int[] getRanges() {
 		
-		for(int i = varIndex; i < firstLine.length; i += numberOfRVars) { 	//percorrer instantes de tempo da RVar
-			for(int j = 1; j <= numberOfSubjects; j++) { 					//percorrer amostras da RVar
-				sample = Integer.parseInt(fileReader.getPosition(j, i)); 	//recolher amostra da linha j e da coluna i
-				if(sample > max) {
-					max = sample; 
+		int numberOfSubjects = subjectCount();
+		int numberOfRVars = randomVarCount() / timeInstantCount();	// numero de variveis aleatorias num instante de tempo
+		int[] ranges = new int[numberOfRVars];
+		
+		//percorrer todas as random vars
+		for (int varIndex = 0; varIndex < numberOfRVars; varIndex++) {
+			
+			int max = 0;	// valor maximo actual da variavel
+			//percorrer instantes de tempo da RVar
+			for(int i = varIndex; i < firstLine.length; i += numberOfRVars) { 	
+				for(int j = 1; j <= numberOfSubjects; j++) { 					//percorrer amostras da RVar
+					int sample = Integer.parseInt(fileReader.getPosition(j, i)); 	//recolher amostra da linha j e da coluna i
+					if(sample > max) {
+						max = sample; 
+					}
 				}
 			}
+			
+			ranges[varIndex] = max + 1;
+			
 		}
-	
-		return max+1;
+		
+		return ranges;
 	}
 	
 	public TimeSlice getTimeSlice(int time) {
 		int numberOfSubjects = subjectCount();
-		int numberOfRVars = randomVarCount();
+		int numberOfRVars = randomVarCount() / timeInstantCount();	// numero de variaveis num instante de tempo
 		int k = 0;
 		
 		TimeSlice timeSlice = new TimeSlice(numberOfSubjects); //chamar construtor da TimeSlice	
