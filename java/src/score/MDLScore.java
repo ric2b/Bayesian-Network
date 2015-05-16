f8da7baa278dbefba31886d211d54d9d59b10df0
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-05-16 22:40:36 +0100

    MDL implementado e melhoria à conversão de j para J

--- java/src/score/MDLScore.java
+++ java/src/score/MDLScore.java
@@ -6,10 +6,19 @@ import bayessian.RandomVariable;
 
 public class MDLScore extends LLScore {
 	
+	int getNetworkComplexity(BayessianNetwork<? extends RandomVariable> bayessian) {
+		int complexity = 0;
+		
+		for(int i: bayessian) { 
+			complexity += (bayessian.getRange(i) - 1) * bayessian.getParentConfigurationCount(i);
+		}
+		return complexity;
+	}	
+	
 	@Override
 	public double getScore(BayessianNetwork<? extends RandomVariable> bayessian, Dataset dataset) {
 		double score = 0;
-		//score = super.getScore() - log(N)*getNetworkComplexity()/2.0;
+		score = super.getScore(bayessian, dataset) -  Math.log(dataset.size())*getNetworkComplexity(bayessian)/(Math.log(2)*2.0);
 		return score;
 	}
 }
