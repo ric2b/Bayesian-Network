f48442ef089e6a6635ba292b41d326a96990b33e
Margarida Reis <margarida.reis@ist.utl.pt>
2015-05-16 15:52:26 +0100

    corrigido index da Var - começa em 1 e nao em 0

--- java/src/bayessian/TransitionBayessianNetwork.java
+++ java/src/bayessian/TransitionBayessianNetwork.java
@@ -96,7 +96,7 @@ public class TransitionBayessianNetwork<T extends RandomVariable> extends Bayess
 		
 		int i = 0;
 		for(Sample item : dataset) {
-			futureValues[i] = calculateFutureValue(indexOfVar-varCountInTimeT, item); //subtrair numero de RVars para mapear o index de X[t+1] para X[t]
+			futureValues[i] = calculateFutureValue(indexOfVar-varCountInTimeT-1, item); //subtrair numero de RVars para mapear o index de X[t+1] para X[t]
 			i++;
 		}
 		
