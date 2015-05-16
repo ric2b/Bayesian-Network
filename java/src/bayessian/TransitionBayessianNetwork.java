c1981b5ddd1dfe7b88fdbfe974c39a4ada4d557e
Margarida Reis <margarida.reis@ist.utl.pt>
2015-05-16 10:11:44 +0100

    adicionado metodo calculateFutureValue()

--- java/src/bayessian/TransitionBayessianNetwork.java
+++ java/src/bayessian/TransitionBayessianNetwork.java
@@ -23,12 +23,12 @@ public class TransitionBayessianNetwork<T> extends BayessianNetwork<RandomVariab
 		return graph.addEdge(vars[srcIndex], vars[destIndex]);
 	}
 	
-	public int calculateProbability(int indexOfVar, int value) {
+	private double calculateSingleProbability(int indexOfVar, int value) {
 		//index = index of X[t+1] do qual queremos saber o valor mais provavel
 		//value = valor que estamos a considerar nesse momento para a RVar que se pretende obter
-		int probability = 0;
+		double probability = 0;
 		int numberOfIterations = 0;
-		int numberOfRvars = vars.length;
+		int numberOfRvars = varCountInTimeT; //dividir por 2 porque metade do array sao variaveis do passado e metade do array sao variaveis do futuro
 		int[] d = new int[numberOfRvars - 1];
 		
 		for(int i = 0; i < numberOfRvars; i++) {
@@ -37,24 +37,40 @@ public class TransitionBayessianNetwork<T> extends BayessianNetwork<RandomVariab
 			}
 		}
 		
-		for(int i = 0; i < numberOfIterations-1; i++) {
+		for(int n = 0; n < numberOfIterations-1; n++) {
 			//calculos
+			//int j = 
+			estimates[indexOfVar].getEstimate(j, value);
 			d[0]++;
-			for(int j = 0; j < numberOfRvars-1; j++) {
-				if(j != indexOfVar) {
-					if(d[j] == getRange(j)) {
-						d[j+1]++;
-						d[j] = 0;
-					}
-					else {
-						break;
-					}
+			for(int m = 0; m < numberOfRvars-1; m++) {
+				if(m == indexOfVar){
+					continue;
+				}
+				if(d[m] == getRange(m)) {
+					d[m+1]++;
+					d[m] = 0;
 				}
 				else {
-					continue;
+					break;
 				}
 			}
 		}
 		return probability;
+	}
+	
+	public int calculateFutureValue(int indexOfVar) {
+		
+		double maxProbability = 0;
+		double probability = 0;
+		int futureValue = 0;
+		
+		for(int m = 0; m < getRange(indexOfVar); m++) {
+			probability = calculateSingleProbability(indexOfVar, m);
+			if(probability > maxProbability) {
+				maxProbability = probability;
+				futureValue = m;
+			}
+		}
+		return futureValue;
 	}
 }
