c25009f9753d6ea0310e723fc1e672794679e034
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-05-18 02:30:18 +0100

    uns ajustes à interface

--- java/src/userinterface/GUI.java
+++ java/src/userinterface/GUI.java
@@ -26,17 +26,22 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import java.awt.Component;
 import javax.swing.DebugGraphics;
+import javax.swing.JProgressBar;
 
 public class GUI {
 
 	private JFrame frame;
 	private JTextField txtHola;
-	private JTextField txtFilenamecsv;
 	private JTextField txtScore;
 	private JTextField txtTestFile;
-	private JTextField textField_1;
 	private JTextField txtRandomRestarts;
 	private JTextField txtInferir;
+	private JTextField txtTotalTime;
+	private JTextField textField;
+	private JTextField textField_2;
+	private JTextField textField_1;
+	private JTextField txtSeconds;
+	private JTextField txtSeconds_1;
 
 	/**
 	 * Launch the application.
@@ -68,66 +73,32 @@ public class GUI {
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("Segoe UI Light", Font.PLAIN, 12));
 		frame.setLocationByPlatform(true);
-		frame.setMaximumSize(new Dimension(400, 400));
-		frame.setMinimumSize(new Dimension(400, 400));
+		frame.setMaximumSize(new Dimension(400, 410));
+		frame.setMinimumSize(new Dimension(400, 435));
 		frame.getContentPane().setLayout(null);
-		
-		txtFilenamecsv = new JTextField();
-		txtFilenamecsv.setEditable(false);
-		txtFilenamecsv.setOpaque(false);
-		txtFilenamecsv.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
-		txtFilenamecsv.setBackground(SystemColor.activeCaptionBorder);
-		txtFilenamecsv.setText("filename.csv");
-		txtFilenamecsv.setBounds(94, 17, 206, 20);
-		frame.getContentPane().add(txtFilenamecsv);
-		txtFilenamecsv.setColumns(10);
 		
 		JRadioButton rdbtnLl = new JRadioButton("LL");
 		rdbtnLl.setToolTipText("log-likelyhood scoring");
 		rdbtnLl.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
 		rdbtnLl.setRequestFocusEnabled(false);
-		rdbtnLl.setBounds(94, 56, 43, 23);
+		rdbtnLl.setBounds(75, 56, 43, 23);
 		frame.getContentPane().add(rdbtnLl);
 		
 		JRadioButton rdbtnMdl = new JRadioButton("MDL");
 		rdbtnMdl.setToolTipText("minimum description length scoring");
 		rdbtnMdl.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
 		rdbtnMdl.setRequestFocusEnabled(false);
-		rdbtnMdl.setBounds(139, 56, 55, 23);
+		rdbtnMdl.setBounds(120, 56, 55, 23);
 		frame.getContentPane().add(rdbtnMdl);
-		
-		JButton btnOpen = new JButton("open");
-		btnOpen.setToolTipText("select the csv file for training");
-		btnOpen.setRequestFocusEnabled(false);
-		btnOpen.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-		btnOpen.setBounds(310, 16, 61, 23);
-		frame.getContentPane().add(btnOpen);
 		
 		JSeparator separator = new JSeparator();
 		separator.setBounds(-19, 189, 403, 2);
 		frame.getContentPane().add(separator);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setOpaque(false);
-		textField_1.setText("filename.csv");
-		textField_1.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
-		textField_1.setColumns(10);
-		textField_1.setBackground(SystemColor.activeCaptionBorder);
-		textField_1.setBounds(94, 202, 206, 20);
-		frame.getContentPane().add(textField_1);
-		
-		JButton button = new JButton("open");
-		button.setToolTipText("select the csv file for testing");
-		button.setRequestFocusEnabled(false);
-		button.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-		button.setBounds(310, 201, 61, 23);
-		frame.getContentPane().add(button);
-		
 		JSpinner spinner = new JSpinner();
 		spinner.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
 		spinner.setModel(new SpinnerNumberModel(new Integer(10), new Integer(0), null, new Integer(1)));
-		spinner.setBounds(328, 58, 43, 18);
+		spinner.setBounds(304, 58, 43, 18);
 		frame.getContentPane().add(spinner);
 		
 		JRadioButton rdbtnTodas = new JRadioButton("all variables");
@@ -152,7 +123,7 @@ public class GUI {
 			}
 		});
 		btnStart.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 16));
-		btnStart.setBounds(152, 312, 79, 38);
+		btnStart.setBounds(152, 347, 79, 38);
 		frame.getContentPane().add(btnStart);
 		
 		txtRandomRestarts = new JTextField();
@@ -163,7 +134,7 @@ public class GUI {
 		txtRandomRestarts.setColumns(10);
 		txtRandomRestarts.setBorder(null);
 		txtRandomRestarts.setBackground(SystemColor.activeCaptionBorder);
-		txtRandomRestarts.setBounds(218, 51, 118, 29);
+		txtRandomRestarts.setBounds(194, 51, 118, 29);
 		frame.getContentPane().add(txtRandomRestarts);
 		
 		JRadioButton rdbtnEscolher = new JRadioButton("choose:");
@@ -230,5 +201,91 @@ public class GUI {
 		btnBuild.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 16));
 		btnBuild.setBounds(152, 140, 79, 38);
 		frame.getContentPane().add(btnBuild);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setValue(30);
+		progressBar.setBounds(29, 103, 146, 14);
+		frame.getContentPane().add(progressBar);
+		
+		txtTotalTime = new JTextField();
+		txtTotalTime.setText("total time: ");
+		txtTotalTime.setRequestFocusEnabled(false);
+		txtTotalTime.setOpaque(false);
+		txtTotalTime.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
+		txtTotalTime.setFocusable(false);
+		txtTotalTime.setColumns(10);
+		txtTotalTime.setBorder(null);
+		txtTotalTime.setBounds(185, 97, 79, 20);
+		frame.getContentPane().add(txtTotalTime);
+		
+		JProgressBar progressBar_1 = new JProgressBar();
+		progressBar_1.setValue(70);
+		progressBar_1.setBounds(29, 309, 146, 14);
+		frame.getContentPane().add(progressBar_1);
+		
+		textField = new JTextField();
+		textField.setText("total time: ");
+		textField.setRequestFocusEnabled(false);
+		textField.setOpaque(false);
+		textField.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
+		textField.setFocusable(false);
+		textField.setColumns(10);
+		textField.setBorder(null);
+		textField.setBounds(185, 303, 79, 20);
+		frame.getContentPane().add(textField);
+		
+		textField_2 = new JTextField();
+		textField_2.setText("filename.csv");
+		textField_2.setOpaque(false);
+		textField_2.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		textField_2.setBackground(SystemColor.activeCaptionBorder);
+		textField_2.setBounds(89, 15, 189, 20);
+		frame.getContentPane().add(textField_2);
+		
+		JButton button_1 = new JButton("open");
+		button_1.setToolTipText("select the csv file for testing");
+		button_1.setRequestFocusEnabled(false);
+		button_1.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
+		button_1.setBounds(286, 14, 61, 22);
+		frame.getContentPane().add(button_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("filename.csv");
+		textField_1.setOpaque(false);
+		textField_1.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
+		textField_1.setEditable(false);
+		textField_1.setColumns(10);
+		textField_1.setBackground(SystemColor.activeCaptionBorder);
+		textField_1.setBounds(90, 201, 189, 20);
+		frame.getContentPane().add(textField_1);
+		
+		JButton button = new JButton("open");
+		button.setToolTipText("select the csv file for testing");
+		button.setRequestFocusEnabled(false);
+		button.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
+		button.setBounds(287, 200, 61, 22);
+		frame.getContentPane().add(button);
+		
+		txtSeconds = new JTextField();
+		txtSeconds.setText("6 seconds");
+		txtSeconds.setOpaque(false);
+		txtSeconds.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
+		txtSeconds.setEditable(false);
+		txtSeconds.setColumns(10);
+		txtSeconds.setBackground(SystemColor.activeCaptionBorder);
+		txtSeconds.setBounds(253, 97, 94, 20);
+		frame.getContentPane().add(txtSeconds);
+		
+		txtSeconds_1 = new JTextField();
+		txtSeconds_1.setText("2 seconds");
+		txtSeconds_1.setOpaque(false);
+		txtSeconds_1.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 14));
+		txtSeconds_1.setEditable(false);
+		txtSeconds_1.setColumns(10);
+		txtSeconds_1.setBackground(SystemColor.activeCaptionBorder);
+		txtSeconds_1.setBounds(253, 303, 94, 20);
+		frame.getContentPane().add(txtSeconds_1);
 	}
 }
