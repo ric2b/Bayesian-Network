aa2764439dc273d76ffe19f22e3360139cb4b915
davidfialho <fialho.david@gmail.com>
2015-05-19 04:23:17 +0100

    javadocs das classes Sample e TimeSlice

--- java/src/dataset/TimeSlice.java
+++ java/src/dataset/TimeSlice.java
@@ -8,33 +8,52 @@ public class TimeSlice implements Iterable<Sample> {
 	private Sample[] timeSlice = null;
 	private int k = -1;
 	
+	/**
+	 * Constructs a time slice with @numberOfSamples maximum number of samples.
+	 * @param numberOfSamples	maximum number of samples of the time slice
+	 */
 	public TimeSlice(int numberOfSamples) {
 		k = 0; 
 		timeSlice = new Sample[numberOfSamples]; //criar array de Sample, ou seja, um array de array de inteiros
 		// [[0,1,2],[1,3,4]] <-> [[samples of subject1],[samples of subject2]]
 	}
-		
+	
+	/**
+	 * Adds a sample to the time slice. If sample is null it is added an empry sample.
+	 * @param sample sample to be added
+	 * @return tru if the smaple was added 
+	 */
 	public boolean addSample(Sample sample) {
 		if(k == timeSlice.length) {
 			return false;
 		}
-		setSample(k, sample);  // adicionar sample na primeira posicao da TimeSlice que estiver vazia (posicao indicada por k)
+		timeSlice[k] = sample;
 		k++;
 		return true;
 	}
 	
-	public void setSample(int indexOfSample, Sample sample) {
-		timeSlice[indexOfSample] = sample;
-	}
-	
+	/**
+	 * Returns the sample at the given position.
+	 * @param indexOfSample position of the sample
+	 * @return	sample at the given position
+	 */
 	public Sample getSample(int indexOfSample) {
 		return timeSlice[indexOfSample];
 	}	
 	
+	/**
+	 * Returns the number of samples in the time slice.
+	 * @return number of samples in the time slice
+	 */
 	public int getSampleCount() {
 		return k+1;
 	}
 	
+	/**
+	 * Tests if there is a sample at the given position or is an empty sample. 
+	 * @param indexOfSample	position
+	 * @return	true if there is a smaple at the given position
+	 */
 	public boolean hasSample(int indexOfSample) {
 		return indexOfSample < timeSlice.length && timeSlice[indexOfSample] != null;
 	}
@@ -89,12 +108,7 @@ public class TimeSlice implements Iterable<Sample> {
 		}
 		
 	}
-	
-	/**
-	 * Devolve um iterador por todas as amostras que existem no timeslice. Amostras vazias não são 
-	 * consideradas.
-	 * @return	iterador
-	 */
+
 	@Override
 	public Iterator<Sample> iterator() {
 		return new Itr();
